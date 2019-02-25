2e15af71789d425832bf35541ca5cb2870166545
Peter Tran <tranpeter1999@gmail.com>
2019-02-25 12:35:35 -0800

    Add files via upload

--- check if 2 digits match but not exact (2 digit number).py
+++ check if 2 digits match but not exact (2 digit number).py
@@ -1,7 +1,35 @@
 x = int(input("Enter 2 numbers: "))
 y = int(input())
 
+#EXACT MATCH
+#x == y
+
+#2 DIGITS MATCH
 if(x+y)%11 == 0 and (x-y)%11 != 0:
     print("True")
 else:
     print("False")
+
+#1 DIGIT MATCH
+#19
+#95
+
+    #19
+        #19%10  -> 9  *11 -> 99
+        #19//10 -> 1  *11 -> 11
+
+#     (((19 %10)*11)-95)%10 == 0 or |((19 %10)*11)-95| < 10
+#  or (((19//10)*11)-95)%10 == 0 or |((19//10)*11)-95| < 10
+    
+#or
+    
+#19
+#59   (x-y)%10 == 0
+    
+#or
+    
+#25
+#21   |x-y| < 10
+
+#NO MATCH
+#else
