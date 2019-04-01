4e7e5a94b3d0b79f9c81d2eea4bd6eb8e51c93f2
Peter Tran <tranpeter1999@gmail.com>
2019-04-01 12:44:47 -0700

    Add files via upload

---
+++ python curriculum.py
@@ -0,0 +1,45 @@
+#PYTHON CURRICULUM
+
+#Day 1: Basics of Programming
+
+#This is a comment
+'''This is a
+multiline
+comment'''
+
+print("Hello") -> Hello
+input("Enter your name: ") #-> >>>Enter your name: John Doe -> 'John Doe'
+
+#casting:
+
+int(1.9) #-> 1
+float(2) #-> 2.0
+str(2) #-> '2'
+
+#string literals:
+#single quotes, double quotes, triple quotes
+
+print("Lets go") #-> prints 'Lets go'
+print("Let's go") #-> prints 'Let's go'
+print("He said "Let's go"") -> Error
+
+#introducing \
+print("All three ways to encase strings:\' \" \'''") #-> prints 'All three ways to encase strings:' " ''''
+print("I can also print myself: \\") #-> prints 'I can also print myself: \'
+
+#Excercises
+
+#Print out: She said: "I sell sea shells by the sea's shore"
+
+#Print out: '''Hello world! I'm a string made out of triple quotes!'''
+
+
+#variables:
+
+x = 1
+hello = "Hello"
+
+print(str(x)) #-> prints '1' (AS A STRING)
+print(int(hello)) #-> error, invalid type
+
+
